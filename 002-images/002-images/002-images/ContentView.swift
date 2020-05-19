e162b675fbac48a336c3292a005034db5a45e09a
OzkrVO <vasquez.oscar@gmail.com>
2020-05-19 11:21:29 -0500

    Base de la APP 02

---
+++ 002-images/002-images/002-images/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  002-images
+//
+//  Created by Oscar Vasquez Orozco on 19/05/20.
+//  Copyright © 2020 Oscar Vasquez Orozco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
