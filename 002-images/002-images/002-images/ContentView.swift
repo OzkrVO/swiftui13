a65d2f2a45bcc1aeef6eeabb82459c38d6a4e831
OzkrVO <vasquez.oscar@gmail.com>
2020-05-19 14:03:55 -0500

    Se corrigue el comentario

--- 002-images/002-images/002-images/ContentView.swift
+++ 002-images/002-images/002-images/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        // La imagen se agrego em la carpeta Assets.xcassets del lado izquierdo
+        // La imagen se agrego en la carpeta Assets.xcassets del lado izquierdo
         Image("newyork")
        
         
