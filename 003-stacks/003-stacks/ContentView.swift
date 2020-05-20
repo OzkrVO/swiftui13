493f4a8ac7475d2fb57fcfca2090679c230241fe
OzkrVO <vasquez.oscar@gmail.com>
2020-05-20 17:56:48 -0500

    2 VStack dentro de 1 HStack para los 2 primeros planes

--- 003-stacks/003-stacks/ContentView.swift
+++ 003-stacks/003-stacks/ContentView.swift
@@ -14,29 +14,54 @@ struct ContentView: View {
         VStack {
             HeaderView()
             
-            VStack {
+            HStack {
                 
-                Text("Plan Básico")
-                    .font(.system(.title, design: .rounded))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
+                VStack {
+                    
+                    Text("Básico")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Text("9.99 €")
+                        .font(.system(size: 31, weight: .heavy, design: .rounded))
+                        .foregroundColor(.white)
+                    
+                    Text("Un curso incluído")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                .padding(30)
+                .background(Color.green)
+                .cornerRadius(10)
                 
-                Text("9.99 €")
-                    .font(.system(size: 35, weight: .heavy, design: .rounded))
-                    .foregroundColor(.white)
                 
-                Text("Un curso incluído")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                VStack {
+                    
+                    Text("Carrera")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    
+                    Text("29.99 €")
+                        .font(.system(size: 27, weight: .heavy, design: .rounded))
+                        .foregroundColor(.black)
+                    
+                    Text("Toda una carrera")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                    
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                .padding(30)
+                .background(Color(red:230/255, green:230/255, blue:230/255))
+                .cornerRadius(10)
+                             
                 
-
-                
-            }
-            .padding(30)
-            .background(Color.green)
-            .cornerRadius(10)
-  
-            
+                }.padding(.horizontal)
+              
         }
     }
 }
