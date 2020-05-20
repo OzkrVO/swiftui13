96cf0df80e2fb3e873e79f9631d99a0d7552d760
OzkrVO <vasquez.oscar@gmail.com>
2020-05-20 13:08:00 -0500

    Base para la APP 03

---
+++ 003-stacks/003-stacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  003-stacks
+//
+//  Created by Oscar Vasquez Orozco on 20/05/20.
+//  Copyright © 2020 Oscar Vasquez Orozco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
