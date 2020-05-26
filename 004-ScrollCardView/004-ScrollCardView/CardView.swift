4bfefebed72c8f0a158090daa0f4b7a83c7b3e07
OzkrVO <vasquez.oscar@gmail.com>
2020-05-26 13:25:46 -0500

    Agregar la vista CardView

---
+++ 004-ScrollCardView/004-ScrollCardView/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  004-ScrollCardView
+//
+//  Created by Oscar Vasquez Orozco on 26/05/20.
+//  Copyright © 2020 Oscar Vasquez Orozco. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
