3bce344aec54dda4e551c3e06833e0e140c55c37
OzkrVO <vasquez.oscar@gmail.com>
2020-05-26 13:20:14 -0500

    Base de la APP 04

---
+++ 004-ScrollCardView/004-ScrollCardView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  004-ScrollCardView
+//
+//  Created by Oscar Vasquez Orozco on 26/05/20.
+//  Copyright © 2020 Oscar Vasquez Orozco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
