294529b3ab0bb204118eb7b2a544e87009429956
OzkrVO <vasquez.oscar@gmail.com>
2020-05-27 20:41:18 -0500

    Base de la APP 05

---
+++ 005-buttons/005-buttons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  005-buttons
+//
+//  Created by Oscar Vasquez Orozco on 27/05/20.
+//  Copyright © 2020 Oscar Vasquez Orozco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
